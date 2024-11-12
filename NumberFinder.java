5b5c7954e4c34fde572f41929e2cbe7afc7744a9
iDizZ1 <87584294+iDizZ1@users.noreply.github.com>
2024-11-12 03:43:26 +0300

    Add files via upload

--- NumberFinder.java
+++ NumberFinder.java
@@ -5,8 +5,13 @@ public class NumberFinder {
             String text = "The price of the product is $19.99";
             Pattern pattern = Pattern.compile("\\d+\\.\\d+");
             Matcher matcher = pattern.matcher(text);
+            boolean find = false;
             while (matcher.find()) {
                 System.out.println(matcher.group());
+                find = true;
+            }
+            if (!find) {
+                System.out.println("Числа не найдены");
             }
         }catch (Exception e){
             System.out.println(e.getMessage());
