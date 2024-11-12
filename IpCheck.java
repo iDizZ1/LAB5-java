3400b26f4cf7093e3e3dbc6f53539ab0fec87898
iDizZ1 <87584294+iDizZ1@users.noreply.github.com>
2024-11-12 03:51:07 +0300

    Add files via upload

--- IpCheck.java
+++ IpCheck.java
@@ -7,7 +7,14 @@ public class IpCheck {
             String IP = "191.168.1.255";
             Pattern pattern = Pattern.compile("((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])");
             Matcher matcher = pattern.matcher(IP);
-            System.out.println(matcher.matches());
+            boolean find = false;
+            while (matcher.find()) {
+                System.out.println(matcher.group());
+                find = true;
+            }
+            if (!find) {
+                System.out.println("IP не найден");
+            }
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
